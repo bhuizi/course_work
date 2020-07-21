dc1c285fec7a30ec1c446285c0af53ebf2c27747
Bryan Huizi <bryan.huizi@magnet360.com>
2020-07-21 14:14:46 -0700

    initial commit

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Root = () => {
+  return (
+    <div>course work</div>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector("#root"));
